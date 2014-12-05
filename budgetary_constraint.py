16bc11cce1639d95d9d8ef9102ce163e024e125a
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2014-12-05 15:52:08 +0100

    Fix code ad produce figures

--- budgetary_constraint.py
+++ budgetary_constraint.py
@@ -45,10 +45,24 @@ from openfisca_france.tests import base
 from openfisca_core.rates import average_rate, marginal_rate
 
 
+
+import rpy2.robjects as robj
+import rpy2.robjects.pandas2ri # for dataframe conversion
+from rpy2.robjects.packages import importr
+# convert the pandas df to an R dataframe
+robj.pandas2ri.activate()
+
+
+# you can even save the output once you like it
+
+
+
+
 def celibataire(year, sali):
     return dict(
         parent1 = dict(birth = datetime.date(year - 40, 1, 1), sali = sali),
         menage = dict(so = 4),
+        label = u"Célibataire",
         )
 
 
@@ -59,6 +73,7 @@ def parent_isole(year, sali):
             dict(birth = datetime.date(year - 9, 1, 1)),
             ],
         menage = dict(so = 4),
+        label = u"Parent isolé",
         )
 
 
@@ -71,6 +86,7 @@ def couple_sans_enfant(year, sali):
             ),
         parent2 = dict(birth = datetime.date(year - 40, 1, 1)),
         menage = dict(so = 4),
+        label = u"Couple",
         )
 
 
@@ -90,12 +106,13 @@ def couple_deux_enfants(year, sali):
             dict(birth = datetime.date(year - 9, 1, 1)),
             ],
         menage = dict(so = 4),
+        label = u"Couple avec deux enfants",
         )
 
 
 def make_result(year, count, sali, menage):
     single_entity_kwargs = menage(year, sali)
-    filename = "{}_{}".format(menage.__name__, year)
+    label = single_entity_kwargs.pop("label")
     single_entity_kwargs.update(
         dict(
             axes = [
@@ -131,8 +148,12 @@ def make_result(year, count, sali, menage):
         })
     N = count - 1
     df = pd.concat([df2[0:N], df1], axis = 1)
-    df[u"Salaire imposable"] = sali
-    df[u"Type de ménage"] = menage.__name__
+    df["sal"] = sali
+    df["typmen"] = menage.__name__
+    df["typmensal"] = "{} et {} euros de salaire imposable".format(label, sali)
+
+#    df[u"Salaire imposable"] = sali
+#    df[u"Type de ménage"] = menage.__name__
     return df
 
 
@@ -162,40 +183,73 @@ if __name__ == '__main__':
     graph_list = [
         dict(
             menages = [celibataire, parent_isole],
-            salis = [0, 8000, 16000],
+            salis = [0, 10000],
             ),
         dict(
             menages = [couple_sans_enfant, couple_deux_enfants],
-            salis = [0, 10000, 20000],
+            salis = [0, 10000],
             ),
         ]
 
-    for graph in graph_list:
+    for graph, filename in zip(graph_list, ['celib', 'couple']):
+        print filename
+        print graph
         df = make_df_final3(
             year = 2011, count = 100, salis = graph["salis"], menages = graph["menages"],
             )
+
+
     #    max_xy_value = min(df_celib.loyer.max(), df_celib.al.max())
-        max_xy_value = 1000
-        graph = (
-            ggplot(
-                aes(
-                    x = 'loyer',
-                    y = 'al',
-                    color = u"Salaire imposable",
-                    linetype = u"Type de ménage",
-                    ),
-                data = df,
-                ) +
-            geom_line() +
-    #        scale_color_manual(values = ["red"] + ["green"] + ["blue"]) +
-            xlab("Loyer mensuel") +
-            ylab("Allocation logement mensuelle") +
-            xlim(low = 0, high = max_xy_value) +
-            ylim(low = 0, high = max_xy_value) +
-            ggtitle("2010") +
-            theme_matplotlib()
-            )
-        print graph
+#        max_xy_value = 1000
+#        graph = (
+#            ggplot(
+#                aes(
+#                    x = 'loyer',
+#                    y = 'al',
+#                    color = u"Salaire imposable",
+#                    linetype = u"Type de ménage",
+#                    ),
+#                data = df,
+#                ) +
+#            geom_line() +
+#    #        scale_color_manual(values = ["red"] + ["green"] + ["blue"]) +
+#            xlab("Loyer mensuel") +
+#            ylab("Allocation logement mensuelle") +
+#            xlim(low = 0, high = max_xy_value) +
+#            ylim(low = 0, high = max_xy_value) +
+#            ggtitle("2010") +
+#            theme_matplotlib()
+#            )
+#        print graph
 
-#        ggsave(graph, file="length.pdf")
+#
+        df_R = robj.conversion.py2ri(df)
 
+        # import graphics devices. This is necessary to shut the graph off
+        # otherwise it just hangs and freezes python
+        gr = importr('grDevices')
+
+        plotFunc_2 = robj.r("""
+        library(ggplot2)
+        library(grid)
+        function(df, filename){
+         max_xy_value = 500
+         df$sal <- as.factor(df$sal)
+         df$typmen <- as.factor(df$typmen)
+         df$typmensal <- as.factor(df$typmensal)
+         graph = ggplot(aes(x = loyer, y = al, color = typmensal, linetype = typmensal), data = df) + geom_line() +
+         scale_color_manual(values = c("red", "red", "green", "green")) +
+         scale_linetype_manual(values = c("solid", "dashed", "solid", "dashed")) +
+         xlab("Loyer mensuel") +
+         ylab("Allocation logement mensuelle") +
+         scale_x_continuous(limits=c(0, max_xy_value), expand = c(0, 0)) +
+         scale_y_continuous(limits=c(0, max_xy_value), expand = c(0, 0)) +
+         guides(color = guide_legend(ncol=1)) +
+         theme_bw() +
+         theme(legend.position = "bottom", legend.title = element_blank(),
+               legend.key = element_blank())
+
+         ggsave(filename, plot = graph, width = 5, scale =1)
+         }
+        """)
+        plotFunc_2(df_R, "{}.pdf".format(filename))
